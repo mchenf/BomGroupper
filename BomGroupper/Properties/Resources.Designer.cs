8fe0e6f89d06313793e0adffd40dd6f68f381186
mchenf <mochen@foss.dk>
2023-07-18 18:09:01 +0800

    Added level size button

--- BomGroupper/Properties/Resources.Designer.cs
+++ BomGroupper/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace BomGroupper.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_font_size_96 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-font-size-96", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
