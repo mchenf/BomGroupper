a758230f230b9897b0646181e9e98b5891ac9287
mchenf <mochen@foss.dk>
2023-07-19 10:40:01 +0800

    Passed test cases

--- BomGroupperTests/LevelGeneratorTests.cs
+++ BomGroupperTests/LevelGeneratorTests.cs
@@ -23,5 +23,20 @@ namespace BomGroupper.Tests
             string actual = LevelGenerator.GetLevelString(input);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        [DataRow(1, ".1")]
+        [DataRow(3, "...3")]
+        [DataRow(6, "......6")]
+        [DataRow(0, "")]
+        [DataRow(13, ".............13")]
+        [DataRow(113, ".................................................................................................................113")]
+        public void TryParseLevelTest(int expected, string input)
+        {
+            if(LevelGenerator.TryParseLevel(input, out int actual))
+            {
+                Assert.AreEqual(expected, actual);
+            }
+        }
     }
 }
