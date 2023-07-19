cb2e44444653477a6fa427312d25ce93187f1e33
mchenf <mochen@foss.dk>
2023-07-19 09:59:42 +0800

    Created unit testing for the level string generator class

---
+++ BomGroupperTests/LevelGeneratorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BomGroupper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomGroupper.Tests
+{
+    [TestClass()]
+    public class LevelGeneratorTests
+    {
+        [TestMethod()]
+        [DataRow(2, ".1")]
+        [DataRow(4, "...3")]
+        [DataRow(7, "......6")]
+        [DataRow(0, "")]
+        public void GetLevelStringTest(int input, string expected)
+        {
+            string actual = LevelGenerator.GetLevelString(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
